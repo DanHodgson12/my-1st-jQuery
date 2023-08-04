0164fbd335e0ca34d5be12ecaf2dc845a1f8c88e
DanHodgson12 <>
2023-08-04 18:31:06 +0000

    added basic jQuery DOM manipulation

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-    $arguments("h2").css("text-decoration", "underline"); // underlines all <h2> elements 
-    $("ul").css("border", "solid 1px #ccc"); // adds border to all lists
+    $arguments("h2").removeClass("underline") // underlines all <h2> elements 
+    $("ul").removeClass("border") // adds border to all lists
 });
